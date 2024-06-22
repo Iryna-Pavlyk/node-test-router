ca83fe7c61568e2836cf5c9d3b953620f6d848b9
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-06-23 00:19:45 +0300

    add http-req: GET, DELETE, PUT, PATCH, POST

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import {
+  createContactsController,
+  deleteContactController,
   getContactByIdController,
   getContactsController,
+  patchContactController,
+  upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import isValidId from '../middlewares/isValidId.js';
 
 const contactsRouter = Router();
 
@@ -11,7 +16,28 @@ contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 
 contactsRouter.get(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(getContactByIdController),
+);
+
+contactsRouter.post('/contacts', ctrlWrapper(createContactsController));
+
+contactsRouter.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
+
+contactsRouter.put(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(upsertContactController),
+);
+
+contactsRouter.patch(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(patchContactController),
 );
 
 export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,30 @@
 import { Contact } from '../db/models/contacts.js';
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
-};
+export const getAllContacts = () => Contact.find();
 
-export const getContactById = async (contactId) => {
-  const contact = await Contact.findById(contactId);
-  return contact;
+// export const getAllContacts = async () => {
+//   const contacts = await Contact.find();
+//   return contacts;
+// };
+
+export const getContactById = (contactId) => Contact.findById(contactId);
+
+export const createContact = (data) => Contact.create(data);
+
+export const deleteContact = (contactId) =>
+  Contact.findOneAndDelete({ _id: contactId });
+
+export const updateContact = async (contactId, data, options = {}) => {
+  const result = await Contact.findByIdAndUpdate({ _id: contactId }, data, {
+    new: true,
+    includeResultMetadata: true,
+    ...options,
+  });
+
+  if (!result || !result.value) return null;
+
+  return {
+    contact: result.value,
+    isNew: Boolean(result?.lastErrorObject?.upserted),
+  };
 };
